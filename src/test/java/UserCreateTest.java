b0591edcf84acbd16136d5766a8e7133d3371d69
shinshr <shinshr@yandex.ru>
2026-05-15 10:08:05 +0300

    Добавил удаление в UserCreateTest после успешного создания пользователя и после создания пользователя с существующими данными

--- src/test/java/UserCreateTest.java
+++ src/test/java/UserCreateTest.java
@@ -1,14 +1,19 @@
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import praktikum.EnvConfig;
 import praktikum.user.UserCreate;
+
+import static user.UserAuthSteps.deleteUser;
+import static user.UserAuthSteps.userLoginAndGetToken;
 import static user.UserSteps.*;
 
 
 public class UserCreateTest {
     private UserCreate user;
+    private String accessToken;
 
     @BeforeEach
     public void setUp() {
@@ -16,10 +21,18 @@ public class UserCreateTest {
         user = UserCreate.random();
     }
 
+    @AfterEach
+    void cleanUp() {
+        if (accessToken != null) {
+            deleteUser(accessToken);
+        }
+    }
+
     @DisplayName("Успешное создание пользователя")
     @Test
     void userCreate() {
-       createUser(user);
+        createUser(user);
+        accessToken = userLoginAndGetToken(user);
     }
 
     @DisplayName("Создание пользователя без почты")
@@ -44,6 +57,7 @@ public class UserCreateTest {
     @Test
     void userCreateAgain() {
         createUser(user);
+        accessToken = userLoginAndGetToken(user);
         createUserWithExistingData(user);
     }
 
